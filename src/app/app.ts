b4c006118a16f4d8ad26a9b4eaf87076d10a1ec9
madu-montes <monteseduarda56@gmail.com>
2026-04-15 22:55:14 -0300

    feat: new components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { Navbar } from './_components/navbar/navbar';
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from './pages/certificados/certificados';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, PrimaryButton, PrimaryButton, SecondaryButton],
+  imports: [Navbar, PrimaryButton, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true
